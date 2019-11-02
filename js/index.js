beb0a125deabde19ebcdac93705f4de61143cb3f
Heber <hebergonza@gmail.com>
2019-11-02 14:45:41 -0400

    added double click, focus and blur

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // Your code goes here
+
+// MOUSEOVER and MOUSELEAVE
+
 const logoHeading = document.querySelector(".logo-heading");
 
 logoHeading.addEventListener("mouseover", () => {
@@ -14,6 +17,8 @@ logoHeading.addEventListener("mouseleave", () => {
 const bodyElement = document.querySelector("body");
 
 
+// KEYDOWN
+
 // keydown should be applied on the document
 // event is also needed in the as a parameter
 document.addEventListener("keydown", (event) => {
@@ -24,6 +29,17 @@ document.addEventListener("keydown", (event) => {
 });
 
 const contentImages = document.querySelectorAll(".img-content img");
+
+// KEYUP
+
+document.addEventListener("keyup", (event) => {
+  if (event.keyCode === 66) {
+    bodyElement.style.fontWeight = "inherit";
+  }
+  return;
+});
+
+// WHEEL
 
 let scale = 1;
 
@@ -43,11 +59,47 @@ contentImages[1].addEventListener("wheel", (event) => {
   contentImages[1].style.transform = `scale(${scale})`;
 });
 
+contentImages.forEach((image) => {
+  image.title = "Scroll to resize"
+});
+
 const mainNav = document.querySelector(".main-navigation");
 mainNav.style.zIndex = "1"; // this was needed, otherwise scaling pic will overlap the navbar
 
+// LOAD
 
 // applied to window
 window.addEventListener("load", () => {
   console.log("Page has fully loaded.");
 })
+
+// preventDefault on a tags
+
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+  })
+})
+
+// DOUBLE CLICKS
+
+let introImage = document.querySelector(".intro img");
+introImage.addEventListener("dblclick", () => {
+  introImage.style.display = "none";
+})
+
+introImage.title = "Double click to remove"
+
+// FOCUS and BLUR
+
+const searchBar = document.createElement("input");
+searchBar.type = "text";
+
+searchBar.style.width = "100px";
+
+const navBar = document.querySelector(".nav");
+
+navBar.style.alignItems = "center";
+
+navBar.append(searchBar);
